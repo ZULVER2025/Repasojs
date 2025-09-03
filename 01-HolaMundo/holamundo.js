4daf3dd15d99d63134b0622738ac85a161f60ef5
Duvan <duvanfelipesanchezvillamil@gmail.com>
2025-09-02 20:13:07 -0500

    repaso java script

---
+++ 01-HolaMundo/holamundo.js
@@ -0,0 +1,31 @@
+console.log('hola mundo');
+
+let cajaDeAndy = "woody";
+
+console.log(cajaDeAndy);
+
+let edad = 25;
+let nombre = "Duvan"
+
+
+console.log('Mi nombre es '+nombre+" y tengo "+edad+"tiernos años");
+
+//array
+
+let frutas = ['manzana','banano','mango']
+
+console.log (frutas);
+
+console.log(frutas[1]);
+
+let frase ="javaScript es extremadamente genial"
+
+console.log (frase);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+// operaciones matematicas
+const suma = 3 +12;
+
+console.log(suma);
